0f1c81381fbf6155a31c5f4474ffdcdb1a4d0795
Ryan Weatherford <ryanweatherford7@gmail.com>
2021-03-11 18:13:24 -0600

    commit

---
+++ NameAndAddressLibrary/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameAndAddressLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel Address { get; set; } = new AddressModel();
+        public string FullAddress => $@"{Address.Street}
+{Address.City}, {Address.State}  {Address.ZipCode}";
+
+
+
+    }
+}
---
+++ NameAndAddressWpfUI/AddressWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using NameAndAddressLibrary;
+
+namespace NameAndAddressWpfUI
+{
+    /// <summary>
+    /// Interaction logic for AddressWindow.xaml
+    /// </summary>
+    /// 
+    public partial class AddressWindow : Window
+    {
+        MainWindow _window = new MainWindow();
+        PersonModel _person = new PersonModel();
+
+        public AddressWindow(MainWindow window, PersonModel person)
+        {
+            InitializeComponent();
+            _window = window;
+            _person = person;
+        }
+
+        private void btnDone_Click(object sender, RoutedEventArgs e)
+        {
+            _person.Address.Street = tBStreet.Text;
+            _person.Address.City = tBCity.Text;
+            _person.Address.State = tBState.Text;
+            _person.Address.ZipCode = tBZipCode.Text;
+            _window.peopleList.Add(_person);
+            _window.Activate();
+            _window.tBFirstName.Text = "";
+            _window.tBLastName.Text = "";
+            this.Close();
+        }
+    }
+}
